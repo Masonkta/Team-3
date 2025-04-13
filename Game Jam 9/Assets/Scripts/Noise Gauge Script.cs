c55c7e4db8e8586d13c137e380f2d848bbc958d2
Masonkta <88817850+Masonkta@users.noreply.github.com>
2025-04-13 12:14:42 -0500

    Noise Level UI level 1 all set

---
+++ Game Jam 9/Assets/Scripts/Noise Gauge Script.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoiseGaugeScript : MonoBehaviour
+{
+    public Image needle;
+    public PlayerStats stats;
+    void Start()
+    {
+        UpdateNoiseGauge();
+    }
+
+
+    void Update()
+    {
+        UpdateNoiseGauge();
+    }
+
+    void UpdateNoiseGauge()
+    {
+        float noise = stats.noiseLevel;
+        float angle;
+
+        if (noise <= 3.1f)
+        {
+            angle = Mathf.Lerp(30f, -85f, noise / 3.1f);
+        }
+        else if (noise <= 7.1f)
+        {
+            angle = Mathf.Lerp(-85f, -220f, (noise - 3.1f) / (7.1f - 3.1f));
+        }
+        else
+        {
+            angle = -220f;
+        }
+        Debug.Log(angle);
+
+        needle.rectTransform.localRotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+
+}
