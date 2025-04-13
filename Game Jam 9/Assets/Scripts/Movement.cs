f0015c3ea7b693d43a1432ce731d93cac2c8a1c3
Gavin Dominique <gavindom315@gmail.com>
2025-04-12 19:23:20 -0500

    Updated the movement script to work with the new sprites

--- Game Jam 9/Assets/Scripts/Movement.cs
+++ Game Jam 9/Assets/Scripts/Movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Movement : MonoBehaviour
 {
@@ -8,9 +9,9 @@ public class Movement : MonoBehaviour
     private Vector2 moveInput;
     private Vector2 lastMoveDirection = Vector2.down;
 
-    [Header("Eye Tracking")]
-    public Transform eyeTransform;
-    public float eyeRotationSpeed = 5f;
+    [Header("Light Tracking")]
+    public Light2D light2d;
+    //public float eyeRotationSpeed = 5f;
 
     [Header("Noise Settings")]
     public float noisePerSecondWhileMoving = .1f;
@@ -26,6 +27,7 @@ public class Movement : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
+
         if (moveInput != Vector2.zero)
         {
             lastMoveDirection = moveInput;
@@ -36,17 +38,34 @@ public class Movement : MonoBehaviour
                     playerStats.AddNoiseWalk(noisePerSecondWhileMoving * Time.deltaTime);
                 }
             }
+
+            float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+            light2d.transform.rotation = Quaternion.Euler(0, 0, angle - 90f); // adjust offset if needed
+
         }
-        UpdateEyeDirection();
+
+        if (moveInput.x < 0)
+        {
+            // goin left
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput.x > 0)
+        {
+            // goin right
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        light2d.transform.localScale = new Vector3(1, 1, 1);
+
     }
     void FixedUpdate()
     {
         rb.linearVelocity = moveInput * moveSpeed;
     }
-    void UpdateEyeDirection()
+    /*void UpdateEyeDirection()
     {
         float angle = Mathf.Atan2(lastMoveDirection.y, lastMoveDirection.x) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
         eyeTransform.rotation = Quaternion.Lerp(eyeTransform.rotation, targetRotation, Time.deltaTime * eyeRotationSpeed);
-    }
+    }*/
 }
